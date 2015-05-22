d64c88b81967509cd7b09704489d7a90994d7dfe
Mikhail <mrmihap@yandex.ru>
2015-05-22 12:11:47 +0300

    Project Created

---
+++ PriceUpdater/PriceUpdater/View/FShowCatalog.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PriceUpdater
+{
+  public partial class FShowCatalog : Form
+  {
+    public FShowCatalog()
+    {
+      InitializeComponent();
+    }
+  }
+}
